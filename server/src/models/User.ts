8d16ee7323edc73b53043d5857f489650944cad9
TWasilonek <tomasz.wasilonek@gmail.com>
2023-12-05 10:02:03 +0100

    changed User id to uuid

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -4,32 +4,32 @@ import { Auditable } from "./Auditable";
 
 @Entity({ name: "users" })
 export class User extends Auditable {
-  @PrimaryGeneratedColumn()
-  id: string;
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
 
-  @Column("varchar", {
-    length: 50,
-    nullable: false,
-  })
-  firstName: string;
+    @Column("varchar", {
+        length: 50,
+        nullable: false,
+    })
+    firstName: string;
 
-  @Column("varchar", {
-    length: 50,
-    nullable: false,
-  })
-  lastName: string;
+    @Column("varchar", {
+        length: 50,
+        nullable: false,
+    })
+    lastName: string;
 
-  @Column("varchar", {
-    length: 255,
-    unique: true,
-    nullable: false,
-  })
-  email: string;
+    @Column("varchar", {
+        length: 255,
+        unique: true,
+        nullable: false,
+    })
+    email: string;
 
-  @Column("varchar", {
-    length: 100,
-    nullable: false,
-  })
-  @Length(8, 100)
-  password: string;
+    @Column("varchar", {
+        length: 100,
+        nullable: false,
+    })
+    @Length(8, 100)
+    password: string;
 }
