cccdc3ff98fc5827ddf084f56ea72706d763ed29
TWasilonek <tomasz.wasilonek@gmail.com>
2024-05-17 11:39:36 +0200

    added node.js polyfills of browser features for the test env

--- lunar-explorer-fe/jest.setup.ts
+++ lunar-explorer-fe/jest.setup.ts
@@ -1,4 +1,19 @@
+import "jest-canvas-mock"; // mock the Canvas API in Node.js env
 import "@testing-library/jest-dom";
+
+// Mock the ResizeObserver API in Node.js env
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
+
+// Mock the fetch API in Node.js env
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+  })
+);
 
 // The below can be used in a Jest global setup file or similar for your testing set-up
 import { loadEnvConfig } from "@next/env";
